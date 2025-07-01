ff9aaf182e0b754f67b01b0b20034ab1d9692c7a
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-07-02 01:29:46 +0900

    とりあえず関数定義してみた

--- src/issue01/mod.rs
+++ src/issue01/mod.rs
@@ -1,2 +1,33 @@
 //! スタンダードなやつ
-//!
+
+use std::fmt::Display;
+
+/// fizzかbuzzかfizzbuzzか数値を表すenum
+///
+/// 3の倍数か5の倍数か3と5の公倍数か、それ以外を表します。
+enum FizzBuzzNumber {
+    Fizz,
+    Buzz,
+    FizzBuzz,
+    Number(u32),
+}
+impl Display for FizzBuzzNumber {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FizzBuzzNumber::Fizz => write!(f, "fizz!"),
+            FizzBuzzNumber::Buzz => write!(f, "buzz!"),
+            FizzBuzzNumber::FizzBuzz => write!(f, "fizzbuzz!"),
+            FizzBuzzNumber::Number(n) => write!(f, "{}", n),
+        }
+    }
+}
+impl FizzBuzzNumber {
+    fn new(n: u32) -> Self {
+        match (n % 3, n % 5) {
+            (0, 0) => Self::FizzBuzz,
+            (0, _) => Self::Fizz,
+            (_, 0) => Self::Buzz,
+            _ => Self::Number(n),
+        }
+    }
+}
